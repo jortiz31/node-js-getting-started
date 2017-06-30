c0bb1b6a42faa815362410e4d76f5321e491d1f9
Joortiz <joortiz@adobe.com>
2017-06-30 13:32:26 -0700

    overhaul

--- public/app.js
+++ public/app.js
@@ -5,45 +5,12 @@ $(document).ready(function() {
   })
 });
 
-$(function() {
-  $(window).scroll(function() {
-    var winTop = $(window).scrollTop();
-    if (winTop >= 30) {
-      $("body").addClass("sticky-shrinknav-wrapper");
-    } else{
-      $("body").removeClass("sticky-shrinknav-wrapper");
-    }
-  });
-});
+let prev = 0;
+let $window = $(window);
+let nav = $('.scrollhide-nav');
 
-$("#homeBtn").click(function(e){
-  e.preventDefault();
-  $.get("home.html", function(data) {
-    $(".results").html(data);
-    console.log('ajax performed');
-  })
-});
-
-$("#aboutBtn").click(function(e){
-  e.preventDefault();
-  $.get("about.html", function(data) {
-    $(".results").html(data);
-    console.log('ajax performed');
-  })
-});
-
-$("#galleryBtn").click(function(e){
-  e.preventDefault();
-  $.get("gallery.html", function(data) {
-    $(".results").html(data);
-    console.log('ajax performed');
-  })
-});
-
-$("#contactBtn").click(function(e){
-  e.preventDefault();
-  $.get("contact.html", function(data) {
-    $(".results").html(data);
-    console.log('ajax performed');
-  })
+$window.on('scroll', function(){
+  const scrollTop = $window.scrollTop();
+  nav.toggleClass('hidden', scrollTop > prev);
+  prev = scrollTop;
 });
